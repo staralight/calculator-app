f42769a7c9630cc9c4456fb597924e442bc108b0
ananda tara <80337102+staralight@users.noreply.github.com>
2022-01-07 20:15:30 +0700

    up

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,7 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
-		int a = 12;
+		int a = 10;
 		int b = 6;
 		
 		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
